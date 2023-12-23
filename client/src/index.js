64e62c4b8a9d78c619b6f2d5dbe970315531508d
nazat47 <nazat.mf@gmail.com>
2023-12-24 00:40:17 +0600

    newv2

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { persistor,store } from "./redux/store";
+import { persistor, store } from "./redux/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") {
+  disableReactDevTools();
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <App />
-      </PersistGate>
+    </PersistGate>
   </Provider>
 );
 
